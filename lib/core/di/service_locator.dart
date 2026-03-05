2ec706153731b5de0ac1a0255afa4e7b921a5de0
Muhammadiy macOS <diy.apple1205@gmail.com>
2026-03-05 12:24:52 +0500

    feat(characters): implement repository

--- lib/core/di/service_locator.dart
+++ lib/core/di/service_locator.dart
@@ -1,9 +1,28 @@
 import 'package:get_it/get_it.dart';
+
 import '/core/network/dio_client.dart';
+import '/features/characters/data/datasources/character_local_datasource.dart';
+import '/features/characters/data/datasources/character_remote_datasource.dart';
+import '/features/characters/data/repositories/character_repository_impl.dart';
+import '/features/characters/domain/repositories/character_repository.dart';
 
 final sl = GetIt.instance;
 
 Future<void> initServiceLocator() async {
   // Network
   sl.registerLazySingleton<DioClient>(() => DioClient());
+
+  // Datasources
+  sl.registerLazySingleton<CharacterRemoteDataSource>(
+    () => CharacterRemoteDataSourceImpl(dioClient: sl()),
+  );
+  sl.registerLazySingleton<CharacterLocalDataSource>(
+    () => CharacterLocalDataSourceImpl(),
+  );
+
+  // Repositories
+  sl.registerLazySingleton<CharacterRepository>(
+    () =>
+        CharacterRepositoryImpl(remoteDataSource: sl(), localDataSource: sl()),
+  );
 }
---
+++ lib/features/characters/domain/repositories/character_repository.dart
@@ -0,0 +1,5 @@
+import '/features/characters/domain/models/character.dart';
+
+abstract class CharacterRepository {
+  Future<List<Character>> getCharacters(int page);
+}
